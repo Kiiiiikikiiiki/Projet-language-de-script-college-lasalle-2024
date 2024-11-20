2ddee4ff4a41e02327c2b46310c2acf29ed8a575
AlejoBerrioC <alejoberrio0105@gmail.com>
2024-11-20 09:18:36 -0500

    new members

--- backend/Database/donnees_initiales.py
+++ backend/Database/donnees_initiales.py
@@ -1,5 +1,5 @@
 import dbFunc 
-from backend.Class_Domain.User import User
+from Class_Domain.User import User
 
 conn = dbFunc.obtenir_connection("backend/Database/munchyroll.db")
 
@@ -11,6 +11,8 @@ dbFunc.add_admin(conn, "Law", User.get_hash_password("5678"))
 #Création des membres
 dbFunc.add_member(conn, "Alejo0105", User.get_hash_password("Berrio"))
 dbFunc.add_member(conn, "Gino12", User.get_hash_password("Noel"))
-dbFunc.add
+dbFunc.add_member(conn, "Justin2004", User.get_hash_password("Coco"))
+dbFunc.add_member(conn, "Diego1984", User.get_hash_password("Cosmo"))
+dbFunc.add_member(conn, "User345", User.get_hash_password("User"))
 
 
