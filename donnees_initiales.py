21c55a04b38c0efc915b0c095e28f5191108ebca
AlejoBerrioC <alejoberrio0105@gmail.com>
2024-11-22 14:08:26 -0500

    Finish data test (I hate us)

--- donnees_initiales.py
+++ donnees_initiales.py
@@ -303,3 +303,166 @@ DandaDan S1 E1 - 4.1
 
 
 """Ajouter commentaires"""
+"""
+Episodes avec des commentaires test
+ID=1 Demon Slayer: Kimetsu no Yaiba S1 E1
+ID=6 Demon Slayer: Kimetsu no Yaiba S2 E2
+ID=13 Attack on Titan S2 E1
+ID=16 Attack on Titan S3 E2
+ID=20 Dragon Ball Super S1 E4
+ID=23 One Piece S1 E3
+ID=25 One Piece S2 E1
+ID=28 One Piece S3 E1
+ID=31 DandaDan S1 E2
+"""
+
+"""Episode ID=1 Demon Slayer: Kimetsu no Yaiba S1 E1"""
+# CommentDAO.add_comment(conn, "Great start to the series!", 1, 1, 12, 0) #ID 1
+# CommentDAO.add_comment(conn, "Animation quality is amazing.", 2, 1, 10, 1) #ID 2
+# CommentDAO.add_comment(conn, "The story feels very engaging so far.", 3, 1, 9, 0)  #ID 3
+# CommentDAO.add_comment(conn, "Tanjiro's character is very likable.", 4, 1, 8, 0)  #ID 4
+# CommentDAO.add_comment(conn, "Loved the emotional opening.", 5, 1, 15, 2)  #ID 5
+
+#Replies
+# CommentDAO.add_comment(conn, "Absolutely agree, it's top-notch!", 3, 1, 4, 0)  
+# CommentDAO.add_comment(conn, "Yeah, the animation really stands out.", 2, 1, 5, 0) 
+# CommentDAO.add_comment(conn, "I teared up a bit too, not gonna lie.", 1, 1, 3, 1)  
+
+# CommentCommentDAO.add_commentComment(conn, 1, 6)
+# CommentCommentDAO.add_commentComment(conn, 2, 7)
+# CommentCommentDAO.add_commentComment(conn, 5, 8)
+
+"""Episode ID=6 Demon Slayer: Kimetsu no Yaiba S2 E2"""
+# CommentDAO.add_comment(conn, "This season's pace is much better.", 1, 6, 13, 1)  # ID 9
+# CommentDAO.add_comment(conn, "Loved the new villain introduction.", 2, 6, 11, 0)  # ID 10
+# CommentDAO.add_comment(conn, "Tanjiro's development is so satisfying.", 3, 6, 10, 0)  # ID 11
+# CommentDAO.add_comment(conn, "Nezuko stole the show in this one!", 4, 6, 9, 1)  # ID 12
+# CommentDAO.add_comment(conn, "This is shaping up to be even better than S1.", 5, 6, 14, 0) # ID 13
+
+# Replies
+# CommentDAO.add_comment(conn, "The pacing makes it way more enjoyable.", 1, 6, 4, 0)  
+# CommentDAO.add_comment(conn, "Nezuko's moments were epic, agreed.", 3, 6, 6, 1)  
+# CommentDAO.add_comment(conn, "Villain introductions are always a highlight.", 2, 6, 5, 0)  
+
+# CommentCommentDAO.add_commentComment(conn, 9, 14)
+# CommentCommentDAO.add_commentComment(conn, 12, 15)
+# CommentCommentDAO.add_commentComment(conn, 10, 16)
+
+
+"""Episode ID=13 Attack on Titan S2 E1"""
+# CommentDAO.add_comment(conn, "The suspense was killing me!", 1, 13, 12, 0)  # ID 17
+# CommentDAO.add_comment(conn, "Eren is such a compelling protagonist.", 2, 13, 9, 0)  # ID 18
+# CommentDAO.add_comment(conn, "The Titans are as terrifying as ever.", 3, 13, 11, 1)  # ID 19
+# CommentDAO.add_comment(conn, "The OST gave me chills!", 4, 13, 15, 0)  # ID 20
+# CommentDAO.add_comment(conn, "This episode's ending was wild.", 5, 13, 13, 0)  # ID 21
+
+# Replies
+# CommentDAO.add_comment(conn, "Totally, the music added so much tension.", 1, 13, 5, 0)  
+# CommentDAO.add_comment(conn, "Eren's choices are always fascinating to watch.", 2, 13, 7, 1)  
+# CommentDAO.add_comment(conn, "That cliffhanger was insane!", 3, 13, 6, 0)  
+
+# CommentCommentDAO.add_commentComment(conn, 20, 22)
+# CommentCommentDAO.add_commentComment(conn, 18, 23)
+# CommentCommentDAO.add_commentComment(conn, 21, 24)
+
+
+"""Episode ID=16 Attack on Titan S3 E2"""
+# CommentDAO.add_comment(conn, "Best episode of the season so far.", 1, 16, 14, 0)  # ID 25
+# CommentDAO.add_comment(conn, "Levi is a total beast!", 2, 16, 16, 0)  # ID 26
+# CommentDAO.add_comment(conn, "The twists just keep getting better.", 3, 16, 12, 1)  # ID 27
+# CommentDAO.add_comment(conn, "Every moment felt so intense.", 4, 16, 10, 0)  # ID 28
+# CommentDAO.add_comment(conn, "Can't wait to see what happens next.", 5, 16, 17, 0)  # ID 29
+
+# # Replies
+# CommentDAO.add_comment(conn, "Levi never fails to impress.", 1, 16, 5, 0)  
+# CommentDAO.add_comment(conn, "I was on the edge of my seat too!", 2, 16, 4, 0)  
+# CommentDAO.add_comment(conn, "This episode was a rollercoaster.", 3, 16, 6, 1)  
+
+# CommentCommentDAO.add_commentComment(conn, 26, 30)
+# CommentCommentDAO.add_commentComment(conn, 28, 31)
+# CommentCommentDAO.add_commentComment(conn, 27, 32)
+
+
+"""Episode ID=20 Dragon Ball Super S1 E4"""
+# CommentDAO.add_comment(conn, "Classic Dragon Ball fun.", 1, 20, 9, 0)  # ID 33
+# CommentDAO.add_comment(conn, "Goku always manages to make me laugh.", 2, 20, 8, 0)  # ID 34
+# CommentDAO.add_comment(conn, "The action scenes were incredible!", 3, 20, 12, 1)  # ID 35
+# CommentDAO.add_comment(conn, "Great mix of humor and action.", 4, 20, 7, 0)  # ID 36
+# CommentDAO.add_comment(conn, "Dragon Ball never disappoints.", 5, 20, 10, 0)  # ID 37
+
+# # Replies
+# CommentDAO.add_comment(conn, "Absolutely, the humor was spot-on.", 1, 20, 3, 0)  
+# CommentDAO.add_comment(conn, "I loved the action too, classic Goku.", 2, 20, 5, 0)  
+# CommentDAO.add_comment(conn, "DB nostalgia hits hard.", 3, 20, 4, 0)  
+
+# CommentCommentDAO.add_commentComment(conn, 36, 38)
+# CommentCommentDAO.add_commentComment(conn, 35, 39)
+# CommentCommentDAO.add_commentComment(conn, 33, 40)
+
+
+"""Episode ID=23 One Piece S1 E3"""
+# CommentDAO.add_comment(conn, "The humor in this episode was spot-on.", 1, 23, 11, 0)  # ID 41
+# CommentDAO.add_comment(conn, "Luffy's antics never get old.", 2, 23, 14, 0)           # ID 42
+# CommentDAO.add_comment(conn, "The crew's dynamic is so entertaining!", 3, 23, 10, 0)  # ID 43
+# CommentDAO.add_comment(conn, "Loved the pacing of this episode.", 4, 23, 8, 0)        # ID 44
+# CommentDAO.add_comment(conn, "The ending scene was so emotional.", 5, 23, 15, 0)      # ID 45
+
+# # Replies
+# CommentDAO.add_comment(conn, "Totally agree, especially Luffy's moments!", 3, 23, 6, 0)  
+# CommentDAO.add_comment(conn, "Their chemistry makes every episode better.", 4, 23, 5, 0)  
+# CommentDAO.add_comment(conn, "The ending hit me hard too, very touching.", 2, 23, 7, 0)  
+
+# CommentCommentDAO.add_commentComment(conn, 42, 46)
+# CommentCommentDAO.add_commentComment(conn, 43, 47)
+# CommentCommentDAO.add_commentComment(conn, 45, 48)
+
+
+"""Episode ID=25 One Piece S2 E1"""
+# CommentDAO.add_comment(conn, "Great start to this arc, can't wait for more!", 1, 25, 12, 0)  # ID 49
+# CommentDAO.add_comment(conn, "Sanji's cooking scene was hilarious!", 2, 25, 9, 0)           # ID 50
+# CommentDAO.add_comment(conn, "The fight choreography was amazing!", 3, 25, 13, 0)           # ID 51
+# CommentDAO.add_comment(conn, "The foreshadowing is very well done.", 4, 25, 11, 0)          # ID 52
+# CommentDAO.add_comment(conn, "This arc has so much potential.", 5, 25, 10, 0)               # ID 53
+
+# # Replies
+# CommentDAO.add_comment(conn, "Sanji always makes me laugh too!", 1, 25, 6, 0)  
+# CommentDAO.add_comment(conn, "Absolutely, the fights are top-notch.", 3, 25, 8, 0)  
+# CommentDAO.add_comment(conn, "I love how they hint at future events subtly.", 5, 25, 7, 0)  
+
+# CommentCommentDAO.add_commentComment(conn, 50, 54)
+# CommentCommentDAO.add_commentComment(conn, 51, 55)
+# CommentCommentDAO.add_commentComment(conn, 52, 56)
+
+
+"""Episode ID=28 One Piece S3 E1"""
+# CommentDAO.add_comment(conn, "The Straw Hat crew is unstoppable!", 1, 28, 13, 0)    # ID 57
+# CommentDAO.add_comment(conn, "The villain introduction was perfect.", 2, 28, 11, 0)  # ID 58
+# CommentDAO.add_comment(conn, "Usopp's moments were hilarious!", 3, 28, 10, 0)       # ID 59
+# CommentDAO.add_comment(conn, "Nami's character is growing a lot.", 4, 28, 12, 0)    # ID 60
+# CommentDAO.add_comment(conn, "This episode was pure One Piece goodness.", 5, 28, 14, 0)  # ID 61
+
+# # Replies
+# CommentDAO.add_comment(conn, "Their teamwork is unbeatable.", 2, 28, 7, 0)  
+# CommentDAO.add_comment(conn, "I agree, the villain feels very threatening.", 1, 28, 8, 0)  
+# CommentDAO.add_comment(conn, "Nami has really come a long way as a character.", 4, 28, 6, 0)  
+
+# CommentCommentDAO.add_commentComment(conn, 57, 62)
+# CommentCommentDAO.add_commentComment(conn, 58, 63)
+# CommentCommentDAO.add_commentComment(conn, 60, 64)
+
+
+"""Episode ID=31 DandaDan S1 E2"""
+# CommentDAO.add_comment(conn, "DanDaDan is a fresh and unique series!", 1, 31, 15, 0)  # ID 65
+# CommentDAO.add_comment(conn, "The art style really stands out.", 2, 31, 13, 0)        # ID 66
+# CommentDAO.add_comment(conn, "This episode had some great action scenes.", 3, 31, 12, 0)  # ID 67
+# CommentDAO.add_comment(conn, "I love how weird and fun this show is.", 4, 31, 10, 0)  # ID 68
+# CommentDAO.add_comment(conn, "The pacing is so well-balanced.", 5, 31, 11, 0)         # ID 69
+
+# # Replies
+# CommentDAO.add_comment(conn, "It's definitely one of the most unique shows out there.", 2, 31, 7, 0)  # Responde al comentario ID 65
+# CommentDAO.add_comment(conn, "The action scenes were top-tier, I agree!", 3, 31, 9, 0)  # Responde al comentario ID 67
+# CommentDAO.add_comment(conn, "The humor is weird but works perfectly.", 5, 31, 8, 0)  # Responde al comentario ID 68
+
+# CommentCommentDAO.add_commentComment(conn, 65, 70)
+# CommentCommentDAO.add_commentComment(conn, 67, 71)
+# CommentCommentDAO.add_commentComment(conn, 68, 72)
